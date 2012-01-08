078e9af04d6eeab918cfb254b3c262a5b8074e27
Yusuf Erdogan <erdoganyusuf@gmail.com>
2012-01-08 15:28:00 +0800

    LinkedIn complete

--- public/javascripts/pages/users-getting-started.js
+++ public/javascripts/pages/users-getting-started.js
@@ -39,6 +39,19 @@ Diaspora.Pages.UsersGettingStarted = function() {
       evt.preventDefault();
       
       $(this).data("cachedValue", $(this).val());
+      
+      if( $("#profile_first_name").val() == '' ||
+      	  $("#profile_last_name").val() == '' ||
+	  	  $("#profile_company").val() == '' ||       
+      	  $("#profile_location").val() == '') {
+      	
+      	var message = "Please provide all required fields."
+        Diaspora.page.flashMessages.render({success: false, notice: message});
+      	
+      	return;
+      } 
+      
+      
       $('#edit_profile').submit();
       $('#gs-name-form-spinner').removeClass("hidden");
       //$('.tag_input').submit();
@@ -74,6 +87,35 @@ Diaspora.Pages.UsersGettingStarted = function() {
           $('li.as-result-item').first().click();
         }
       }
-    });
+    });        
   });
 };
+
+function onLinkedInLoad() {
+	
+	IN.Event.on(IN, "auth", function() {onLinkedInLogin();});
+  	IN.Event.on(IN, "logout", function() {onLinkedInLogout();});		
+	
+}
+
+function onLinkedInLogin(){
+	IN.API.Profile("me")
+	.fields(["firstName","lastName","summary","positions", "specialties", "location"])
+    .result(function(result) { 
+    	var profile = result.values[0];
+        $("#profile_first_name").val(profile.firstName);
+        $("#profile_last_name").val(profile.lastName);
+        $("#profile_bio").val(profile.summary + '\n' + profile.specialties);
+        $("#profile_location").val(profile.location.name);
+        
+        $.each(profile.positions.values, function(i, val){
+        	if(val.isCurrent){
+        		$("#profile_company").val(val.company.name);
+        	}
+        })
+        
+        
+    });
+}
+
+function onLinkedInLogout(){}
